9159ccb596d709ab6ce9200c0d2a1d616a5e47eb
abhi2479 <abhi247997@gmail.com>
2023-04-25 15:52:03 +0530

    Till Age Filters (S13.160)

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -18,11 +19,24 @@ namespace API.Data
 
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMemberAsync()
+        public async Task<PagedList<MemberDto>> GetMemberAsync(UserParams userParams)
         {
-            return await _context.Users
-            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-            .ToListAsync();
+            var query = _context.Users.AsQueryable();
+            query= query.Where(u=>u.UserName!=userParams.CurrentUsername);
+            query = query.Where(u=> u.Gender == userParams.Gender);
+
+            var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears
+                (-userParams.MaxAge -1));
+
+            var maxDob= DateOnly.FromDateTime(DateTime.Today.AddYears
+                (-userParams.MinAge));
+
+            query = query.Where(u=>u.DateOfBirth >= minDob && u.DateOfBirth<=maxDob);
+
+            return await  PagedList<MemberDto>.CreateAsync(
+                query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider),
+                userParams.pageNumber,
+                userParams.PageSize);
         }
 
         public async Task<MemberDto> GetMemberAsync(string username)
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -7,6 +7,7 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { FileUploadModule } from 'ng2-file-upload';
 import {BsDatepickerModule} from 'ngx-bootstrap/datepicker'
+import {PaginationModule} from 'ngx-bootstrap/pagination'
 
 
 
@@ -24,7 +25,8 @@ import {BsDatepickerModule} from 'ngx-bootstrap/datepicker'
       type: 'line-scale-party'
     }),
     FileUploadModule,
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot()
   ],
 
   exports:[
@@ -34,7 +36,8 @@ import {BsDatepickerModule} from 'ngx-bootstrap/datepicker'
     NgxGalleryModule,
     NgxSpinnerModule,
     FileUploadModule,
-    BsDatepickerModule
+    BsDatepickerModule,
+    PaginationModule
   ]
 })
 export class SharedModule { }
